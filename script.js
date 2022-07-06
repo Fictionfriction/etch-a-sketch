0a07e5fab7edb3735b04a431de40262a86e1e3a7
Jason Kearney <jkearney345@gmail.com>
2022-07-06 18:05:52 -0400

    change color when hover grid

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ for (let i = 0; i < 256; i++) {
     con.appendChild(div);
 }
 
+function determineW(num) {
+    let newWidth = 500 / num;
+    return newWidth;
+}
+
+function changeColor(e) {
+    this.classList.add('color');
+}
+
+let divs = document.querySelectorAll('.grid');
+divs.forEach(div => div.addEventListener('mouseenter', changeColor));
+
