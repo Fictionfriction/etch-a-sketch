b6ce683811c50df249c8915781d7e18fc6a8ca44
Jason Kearney <jkearney345@gmail.com>
2022-07-06 17:56:13 -0400

    create grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const con = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
+    div.classList.add('grid');
     con.appendChild(div);
 }
 
