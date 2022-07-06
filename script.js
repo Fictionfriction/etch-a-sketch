d2963a08310df82425536847b6417ffd15c48cd3
Jason Kearney <jkearney345@gmail.com>
2022-07-06 18:31:52 -0400

    change grid size based on prompt

--- script.js
+++ script.js
@@ -15,6 +15,40 @@ function changeColor(e) {
     this.classList.add('color');
 }
 
+function promptChange(e) {
+    let newNum = prompt('Enter a number to change grid!');
+    while (newNum >= 100) {
+        newNum = prompt('Less than 100 please!')
+    }
+    createNewGrid(newNum);
+}
+
+function createNewGrid(num) {
+    removeGrid();
+    const width = determineW(num);
+    for (let i = 0; i < (num * num); i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = `${width}px`
+        con.appendChild(div);
+    }
+    addColor();
+}
+
+function removeGrid() {
+    const elements = document.getElementsByClassName('grid');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function addColor() {
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => div.addEventListener('mouseenter', changeColor));
+}
+
 let divs = document.querySelectorAll('.grid');
 divs.forEach(div => div.addEventListener('mouseenter', changeColor));
 
+const button = document.querySelector('button');
+button.addEventListener('click', promptChange);
